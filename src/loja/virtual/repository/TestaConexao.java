1c8bc09023cd1d214a5a306461cc0e589c3bf912
willtsuchida <willian_hideki@hotmail.com>
2023-03-07 13:31:24 -0300

    Cap 1 finalizad, Java e JDBC: trabalhando com um banco de dados

---
+++ src/loja/virtual/repository/TestaConexao.java
@@ -0,0 +1,19 @@
+package loja.virtual.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class TestaConexao {
+    
+    public static void main(String[] args) throws SQLException {
+        
+        Connection connection = DriverManager.getConnection
+                ("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "root");
+        
+        System.out.println("fechando conexao");
+        
+        connection.close();
+    }
+}
