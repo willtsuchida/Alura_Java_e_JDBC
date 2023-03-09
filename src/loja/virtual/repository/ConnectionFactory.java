a3a6c59374daf127739608cc85a6709158f09f28
willtsuchida <willian_hideki@hotmail.com>
2023-03-09 13:19:58 -0300

    Cap 5 finalizaod. Java e JDBC: trabalhando com um banco de dados

--- src/loja/virtual/repository/ConnectionFactory.java
+++ src/loja/virtual/repository/ConnectionFactory.java
@@ -1,14 +1,28 @@
 package loja.virtual.repository;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import javax.sql.DataSource;
 
 public class ConnectionFactory {
-
+    
+    public DataSource dataSource;
+    
+    public ConnectionFactory() {
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
+        comboPooledDataSource.setUser("root");
+        comboPooledDataSource.setPassword("root");
+        
+        comboPooledDataSource.setMaxPoolSize(15);
+        
+        this.dataSource = comboPooledDataSource;
+    }
+    
     public Connection recuperaConexao() throws SQLException {
-
-        return DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "root");
-
+        
+        return this.dataSource.getConnection();
     }
 }
---
+++ src/loja/virtual/repository/TestaPoolConexao.java
@@ -0,0 +1,16 @@
+package loja.virtual.repository;
+
+import java.sql.SQLException;
+
+public class TestaPoolConexao {
+
+    public static void main(String[] args) throws SQLException {
+
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+
+        for (int i = 0; i < 20; i++) {
+            connectionFactory.recuperaConexao();
+            System.out.println("Conexao de numero: " + i);
+        }
+    }
+}
